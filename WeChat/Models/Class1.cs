a4ae8fbeac12a66933114ff48d1945448dd7dbf6
xuanjunL <342216133@qq.com>
2016-07-13 14:53:12 +0800

    数据库test

--- WeChat/Models/Class1.cs
+++ WeChat/Models/Class1.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
 namespace WeChat.Models
 {
     public class Class1
     {
+        public void a()
+        {
+            YummyOnlineManager manager = new YummyOnlineManager();
+
+            manager.GetHotels();
+        }
     }
 }

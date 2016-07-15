941d02a090f440b4da97f40aa85f277e3ab7afa8
JsBlueCat <15618561709@163.com>
2016-07-15 09:29:53 +0800

    合并

---
+++ Management/App_Start/HotelManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Management
+{
+    public class HotelManager:YummyOnlineDAO.YummyOnlineManager
+    {
+        public async Task<List<string>> GetTakeOutDeskes()
+        {
+            return await db
+        }
+    }
+}

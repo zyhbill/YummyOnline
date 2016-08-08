448cbda5529eaa3c4391a99fef60f5107b505055
JsBlueCat <15618561709@163.com>
2016-08-08 16:26:09 +0800

    新增 articles

---
+++ YummyOnlineDAOModels/ArticleModels.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YummyOnlineDAO.Models
+{
+    public enum ArticleStatus
+    {
+        Ungranted  = 0,
+        Granted  =1,
+    }
+    public class Article
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [MaxLength(128),Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string PictruePath { get; set; }
+
+        [Required]
+        public string Discription { get; set; }
+
+        [Required] 
+        public string Body { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime DateTime { get; set; }
+
+        public ArticleStatus Status { get; set; }
+
+        public int HotelId { get; set; }
+        public Hotel Hotel { get; set; }
+
+        public string StaffId { get; set; }
+        public Staff Staff { get; set; }
+    }
+}

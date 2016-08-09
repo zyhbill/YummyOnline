61233681c8daf8346e969070600f702ddd8de7d6
BlueFisher <blue_fisher@qq.com>
2016-08-09 09:08:04 +0800

    修改模型

--- YummyOnlineDAOModels/ArticleModels.cs
+++ YummyOnlineDAOModels/ArticleModels.cs
@@ -6,38 +6,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace YummyOnlineDAO.Models
-{
-    public enum ArticleStatus
-    {
-        Ungranted  = 0,
-        Granted  =1,
-    }
-    public class Article
-    {
-        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-        [MaxLength(128),Required]
-        public string Title { get; set; }
+namespace YummyOnlineDAO.Models {
+	public enum ArticleStatus {
+		Ungranted = 0,
+		Granted = 1,
+	}
+	public class Article {
+		[Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+		[MaxLength(128), Required]
+		public string Title { get; set; }
 
-        [Required]
-        public string PictruePath { get; set; }
+		[Required]
+		public string PictruePath { get; set; }
 
-        [Required]
-        public string Discription { get; set; }
+		[Required]
+		public string Description { get; set; }
 
-        [Required] 
-        public string Body { get; set; }
+		[Required]
+		public string Body { get; set; }
 
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public DateTime DateTime { get; set; }
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public DateTime DateTime { get; set; }
 
-        public ArticleStatus Status { get; set; }
+		public ArticleStatus Status { get; set; }
 
-        public int HotelId { get; set; }
-        public Hotel Hotel { get; set; }
+		[ForeignKey(nameof(Hotel))]
+		public int HotelId { get; set; }
+		public Hotel Hotel { get; set; }
 
-        public string StaffId { get; set; }
-        public Staff Staff { get; set; }
-    }
+		[ForeignKey(nameof(Staff))]
+		public string StaffId { get; set; }
+		public Staff Staff { get; set; }
+	}
 }

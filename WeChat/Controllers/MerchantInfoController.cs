aa5c3e13d33951bc6eab3cb4d208cbd4437bd5ed
xuanjunL <342216133@qq.com>
2016-08-16 11:25:47 +0800

    商户信息 修改

--- WeChat/Controllers/MerchantInfoController.cs
+++ WeChat/Controllers/MerchantInfoController.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YummyOnlineDAO.Models;
 
 namespace WeChat.Controllers
 {
     public class MerchantInfoController : Controller
     {
-        // GET: MerchantInfo
         public ActionResult Index()
         {
             return View("MerchantInfo");
+        }
+
+        // GET: MerchantInfo
+        public ActionResult Info(int article)
+        {
+            Session["id"] = article;
+            var yummonlineManager = new YummyOnlineManager();
+            var ctx = new YummyOnlineContext();
+            var result = ctx.Articles.Where(d => d.Id == article && d.Status == ArticleStatus.Granted).Select(s => new { s.Body,s.Title,s.Description});
+            foreach (var i in result)
+            {
+                ViewData["body"] = i.Body;
+                ViewData["title"] = i.Title;
+                ViewData["des"] = i.Description;
+            }
+            return View("Info");
         }
     }
 }
